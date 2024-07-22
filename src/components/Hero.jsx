bd249250c231e104b92a16cadfd3b8d737b1b15b
monysary <sary.mony@gmail.com>
2024-07-22 08:52:38 -0700

    updated some styling on hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
 
   return (
     <>
-      <div className='relative top-[-72px] md:top-[-80px] mx-auto h-[100vh] md:h-[100vh] flex justify-center items-center overflow-hidden bg-no-repeat bg-center bg-cover bg-fixed' style={{backgroundImage: `url(${BeyondStage})`}}>
+      <div className='relative top-[-72px] md:top-[-80px] mx-auto h-[100vh] overflow-hidden bg-no-repeat bg-center bg-cover bg-fixed' style={{backgroundImage: `url(${BeyondStage})`}}>
       </div>
     </>
   )
