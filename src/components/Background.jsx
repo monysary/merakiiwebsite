e11db44dde6eca9533b2d597054557d7fd2984df
monysary <sary.mony@gmail.com>
2024-11-10 15:41:13 -0800

    removed changing light and dark mode, maintained dark mode css styling

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -2,7 +2,7 @@ export default function Background() {
   return (
     <div className="fixed inset-0 flex justify-center sm:px-8">
       <div className="flex w-full max-w-4xl lg:px-8 border-x-4 border-solid border-fuchsia-800">
-        <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20"></div>
+        <div className="w-full ring-1 bg-zinc-900 ring-zinc-300/20"></div>
       </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -73,28 +73,28 @@ function MobileNavigation(props) {
   return (
     <div className='sticky top-0 pt-6 px-6 z-50 md:hidden'>
       <Popover {...props} className="flex md:hidden justify-end">
-        <PopoverButton className="group flex items-center rounded-full bg-white/90 p-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20">
+        <PopoverButton className="group flex items-center rounded-full p-2 text-sm font-medium shadow-lg shadow-zinc-800/5 ring-1 backdrop-blur bg-zinc-800/90 text-zinc-200 ring-white/10 hover:ring-white/20">
           <img src={Socials_Octopus_Front} alt='Merakii OctopusMenu' className='w-8' />
         </PopoverButton>
         <PopoverBackdrop
           transition
-          className="fixed inset-0 z-50 bg-zinc-800/40 backdrop-blur-sm duration-150 data-[closed]:opacity-0 data-[enter]:ease-out data-[leave]:ease-in dark:bg-black/80"
+          className="fixed inset-0 z-50 backdrop-blur-sm duration-150 data-[closed]:opacity-0 data-[enter]:ease-out data-[leave]:ease-in bg-black/80"
         />
         <PopoverPanel
           focus
           transition
-          className="fixed inset-x-4 top-8 z-50 origin-top rounded-3xl bg-white p-8 ring-1 ring-zinc-900/5 duration-150 data-[closed]:scale-95 data-[closed]:opacity-0 data-[enter]:ease-out data-[leave]:ease-in dark:bg-zinc-900 dark:ring-zinc-800"
+          className="fixed inset-x-4 top-8 z-50 origin-top rounded-3xl p-8 ring-1 duration-150 data-[closed]:scale-95 data-[closed]:opacity-0 data-[enter]:ease-out data-[leave]:ease-in bg-zinc-900 ring-zinc-800"
         >
           <div className="flex flex-row-reverse items-center justify-between">
             <PopoverButton aria-label="Close menu" className="-m-1 p-1">
-              <CloseIcon className="h-6 w-6 text-zinc-500 dark:text-zinc-400" />
+              <CloseIcon className="h-6 w-6 text-zinc-400" />
             </PopoverButton>
             <PopoverButton>
               <MerakiiLogo width={'100px'} />
             </PopoverButton>
           </div>
           <nav className="mt-6">
-            <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
+            <ul className="-my-2 divide-y text-base divide-zinc-100/5 text-zinc-300">
               <MobileNavItem href="#music">MUSIC</MobileNavItem>
               <MobileNavItem href="#video">VIDEO</MobileNavItem>
               <MobileNavItem href="#shows_merch">SHOWS</MobileNavItem>
